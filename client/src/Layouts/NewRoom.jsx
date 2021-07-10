e82cc1df0f7288a657d3a5e24c72f1ce47c4f9bb
kunatastic <kjha6666@gmail.com>
2021-07-10 22:24:50 +0530

    all the beautidul home page done

--- client/src/Layouts/NewRoom.jsx
+++ client/src/Layouts/NewRoom.jsx
@@ -1,18 +1,72 @@
 import React from "react";
-import { CssBaseline, Container, Typography } from "@material-ui/core";
+import { Link } from "react-router-dom";
+// Made by Yago Estévez (Twitter: @yagoestevez.com)
 
-export default function SimpleContainer() {
+/***********************
+  Nav Component
+ ***********************/
+
+const Nav = (props) => {
   return (
     <React.Fragment>
-      <CssBaseline />
-      <Container maxWidth="sm">
-        <Typography
-          component="div"
-          style={{ backgroundColor: "#cfe8fc", height: "100vh" }}
-        >
-          Hello
-        </Typography>
-      </Container>
+      <nav id="navbar">
+        <div className="nav-wrapper">
+          <p className="brand">
+            <Link to="/">
+              My
+              <strong>Dumb</strong>
+              Game
+              <span className="color">.com</span>
+            </Link>
+          </p>
+        </div>
+      </nav>
     </React.Fragment>
+  );
+};
+
+/***********************
+  Header Component
+ ***********************/
+
+const Header = (props) => {
+  return (
+    <header id="welcome-section">
+      <div className="forest" />
+      <div className="silhouette" />
+      <div className="moon" />
+      <div className="container">
+        <h1>
+          <span className="line">Are you as </span>
+          <span className="line">
+            bored as I am <span className="color">?</span>
+          </span>
+          <span className="line">
+            <span className="color">Maybe</span> a game.
+          </span>
+        </h1>
+        <div className="buttons">
+          <Link to="/create" className="button_link">
+            Create a new Room
+          </Link>
+          <Link to="/join" className="cta button_link">
+            Join a Room
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+/***********************
+  Main Component
+ ***********************/
+
+export default function Home() {
+  return (
+    <>
+      <Nav />
+      <Header />
+    </>
   );
 }
